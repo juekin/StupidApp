63b3622e159788a31109f691dcc87dcfe1e38a2d
Kin Jue <jue.kin@gmail.com>
2018-03-17 14:04:12 -0700

    Initial Commit

---
+++ StupidApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StupidApp
+//
+//  Created by Kin Jue on 3/17/18.
+//  Copyright © 2018 Kin Jue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
