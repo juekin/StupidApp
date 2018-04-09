57f44abeef16d0ffb2861dd448ac7a1d4bcdfef4
Kin <jue.kin@gmail.com>
2018-04-08 22:00:06 -0700

    Test Commit

--- StupidApp/ViewController.swift
+++ StupidApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         view.backgroundColor = UIColor.green
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage (sender: UIButton){
+        let alertController = UIAlertController(title: "Welcome to my First App", message: "Hello Warren", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated:true, completion: nil)
+    }
 }
 
